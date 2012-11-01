5c76045a45bbf0a23c6aa2ca563907f7804b69ed
iocast <iocast@me.com>
2012-11-01 22:34:04 +0100

    HTML now supports exception report handling

--- FeatureServer/Service/HTML.py
+++ FeatureServer/Service/HTML.py
@@ -1,36 +1,27 @@
-__author__  = "MetaCarta"
-__copyright__ = "Copyright (c) 2006-2008 MetaCarta"
-__license__ = "Clear BSD" 
-__version__ = "$Id: HTML.py 561 2008-05-23 12:01:15Z crschmidt $"
-
 from FeatureServer.Service.Request import Request
-from Cheetah.Template import Template
+import vectorformats.Formats.HTML
 
-class HTML (Request):
-    default_template = "template/default.html"
-    metadata_template = "templates/layers.html"
-
-    def _datasource (self):
-        return self.service.datasources[self.datasources[0]]
-    
-    def encode_metadata(self, action):
-        layers = self.service.datasources
-        if self.service.metadata.has_key("metadata_template"):
-            self.metadata_template = self.service.metadata['metadata_template']
+class HTML (Request):    
+    #def encode_metadata(self, action):
+    #    layers = self.service.datasources
+    #    if self.service.metadata.has_key("metadata_template"):
+    #        self.metadata_template = self.service.metadata['metadata_template']
             
-        template = file(self.metadata_template).read()
-        output = Template(template, searchList = [{'layers': layers, 'datasource':self.datasources[0]}, self])
-        return  "text/html; charset=utf-8", str(output).decode("utf-8")
+    #    template = file(self.metadata_template).read()
+    #    output = Template(template, searchList = [{'layers': layers, 'datasource':self.datasources[0]}, self])
+    #    return  "text/html; charset=utf-8", str(output).decode("utf-8")
 
     def encode(self, result):
-        template = self.template()
-        output = Template(template, searchList = [{'actions':result, 'datasource':self.datasources[0]}, self])
+        html = vectorformats.Formats.HTML.HTML(datasource=self.service.datasources[self.datasources[0]])
+        
+        output = html.encode(result)
+        
         return ("text/html; charset=utf-8", str(output).decode("utf-8"), None, 'utf-8')
     
-    def template(self, name="default_template"):
-        datasource = self._datasource()
-        if hasattr(datasource, "default_template"):
-            template = datasource.default_template
-        else:
-            template = self.default_template
-        return file(template).read()
+    def encode_exception_report(self, exceptionReport):
+        html = vectorformats.Formats.HTML.HTML()
+        
+        output = html.encode_exception_report(exceptionReport)
+        
+        return ("text/html; charset=utf-8", str(output).decode("utf-8"), None, 'utf-8')
+    
--- vectorformats/Formats/HTML.py
+++ vectorformats/Formats/HTML.py
@@ -6,13 +6,24 @@ from Cheetah.Template import Template
 class HTML (Format):
     """Uses Cheetah to format a list of features."""
 
-    template_file = "template/default.html"
+    default_file = "template/default.html"
+    exception_file = "template/exception_report.html"
     """Default template file to use."""
 
     def encode(self, result, **kwargs):
-        template = self.template()
-        output = Template(template, searchList = [{'features':result}, self])
+        template = self.template(self.default_file)
+
+        output = Template(template, searchList = [{'features':result, 'datasource':self.datasource.name}, self])
+        
+        return str(output)
+
+    def encode_exception_report(self, exceptionReport):
+        template = self.template(self.exception_file)
+        
+        output = Template(template, searchList = [{'exception_report':exceptionReport}, self])
+        
         return str(output)
     
-    def template(self):
-        return file(self.template_file).read()
+    
+    def template(self, template_file):
+        return file(template_file).read()
