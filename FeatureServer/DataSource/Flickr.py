23a05d224b1192812de34c95a3196fc245d7058e
iocast <iocast@me.com>
2012-11-06 13:41:03 +0100

    Adaption for Flickr API

--- FeatureServer/DataSource/Flickr.py
+++ FeatureServer/DataSource/Flickr.py
@@ -1,165 +1,149 @@
-#!/usr/bin/env python
-
-__author__  = "MetaCarta"
-__copyright__ = "Copyright (c) 2006-2008 MetaCarta"
-__license__ = "Clear BSD" 
-__version__ = "$Id: Flickr.py 468 2008-05-18 06:31:59Z crschmidt $"
-
 from FeatureServer.DataSource import DataSource
 from vectorformats.Feature import Feature
+from FeatureServer.Exceptions.NoGeometryException import NoGeometryException
+
 import md5
 import urllib
-import xml.dom.minidom as minidom
+from lxml import etree
 
-FLICKR_API_KEY = "065ba003282b719d84e2a322046c7364"
-FLICKR_SECRET = "a192dec51fb99499"
+from StringIO import StringIO
 
 class Flickr (DataSource):
-    """Datasource for talking to the flickr API."""
-    def __init__(self, name, api_key = None, secret = None, maxpages = 1, maxfeatures = 100, **args):
+
+    def __init__(self, name, api_key, api_secret, attributes = "*", srid_out = 4326, **args):
         DataSource.__init__(self, name, **args)
-        global FLICKR_API_KEY, FLICKR_SECRET
-        self.attributes = ['user_id','tags','tag_mode','text','min_upload_date','max_upload_date','min_taken_date','max_taken_date','license','sort','privacy_filter','accuracy','machine_tags','machine_tag_mode','group_id','per_page']
-        self.queryable   = ','.join(self.attributes)
-        self.api_key     = api_key or FLICKR_API_KEY
-        self.secret      = secret  or FLICKR_SECRET
-        self.maxpages    = maxpages
-        self.maxfeatures = maxfeatures
-        self.api         = API(self.api_key, self.secret)
+        self.api_key    = api_key
+        self.api_secret = api_secret
+        self.srid_out   = srid_out
+        self.attributes = attributes
         
+        self.api        = FlickrAPI(self.api_key, self.api_secret)
+    
     def select (self, action):
-        """Use the flickr.photos.search method to find photos based on bbox/attributes, 
-           or the flickr.photos.getInfo method if given an ID.""" 
         features = [] 
         if action.id is not None:
             data = self.api.request({'method':'flickr.photos.getInfo','photo_id':action.id})
-            doc = minidom.parseString(data)
-            photo = doc.getElementsByTagName("photo")[0]
-            features.append(self.convert_single_photo_xml_to_feature(photo))
-            
+            doc = etree.parse(StringIO(data)).getroot()
+            photo = doc.xpath('/rsp/photo')[0]
+            try:
+                features.append(self.convert_photo(photo))
+            except Exception as e:
+                ''' '''
+        
         else:
-            page = 1 
-            pages = self.maxpages
-            max = action.maxfeatures or self.maxfeatures
-            while page <= pages and len(features) < max:
-                params = {'method':'flickr.photos.search', 'extras':'geo,tags,license', 'page': page, 'per_page': max  }
-                if hasattr(self, 'auth_token'):
-                    params['auth_token'] = self.auth_token
-                for attr in self.attributes:
-                    if hasattr(self, attr):
-                        params[attr] = getattr(self, attr) 
-                    elif action.attributes.has_key(attr):
-                        params[attr] = action.attributes[attr]
-                if action.bbox:
-                    if action.bbox[0] < -180:
-                        action.bbox[0] = -180
-                    if action.bbox[1] < -90:
-                        action.bbox[1] = -90
-                    if action.bbox[2] > 180:
-                        action.bbox[2] = 180
-                    if action.bbox[3] > 90:
-                        action.bbox[3] = 90
-                    params['bbox'] = ','.join(map(str,action.bbox))
-                data = self.api.request(params)
-                doc = minidom.parseString(data)
-                
-                # Determine the number of pages we should iterate through
-                photos_elem = doc.getElementsByTagName("photos")[0]
-                pages_from_xml = int(photos_elem.attributes['pages'].value)
-                if pages_from_xml < pages:
-                    pages = pages_from_xml
-                
-                photos = doc.getElementsByTagName("photo")
-                for photo in photos:
-                    if len(features) == max: continue
-                    feature = self.convert_photo_xml_to_feature(photo)
-                    features.append(feature)
-                page += 1        
-        return features    
-
-    def convert_single_photo_xml_to_feature(self, xml):
-        """Convert the Flickr Photo XML to a Feature object. XML arg is
-           an xml.dom.minidom object. this reads the response from the getInfo
-           method.""" 
-        attrs = xml.attributes.items() 
-        props = {}
-        for attr in attrs:
-            props[attr[0]] = attr[1]
-        
-        props['owner'] = xml.getElementsByTagName("owner")[0].attributes['nsid'].value
-        for i in ['title', 'description']:
-            node = xml.getElementsByTagName(i)[0]
-            if node.firstChild:
-                props[i] = node.firstChild.nodeValue
-        
-        loc = xml.getElementsByTagName("location")[0]
-        coordinates = [float(loc.attributes['longitude'].value), float(loc.attributes['latitude'].value)]
-        return Feature(id=xml.getAttribute("id"), geometry={'type':"Point", 'coordinates':coordinates}, srs=self.srid_out, props=props)
-        
-    def convert_photo_xml_to_feature(self, xml):
-        """Convert the Flickr Photo XML to a Feature object. XML arg is an
-        xml.dom.minidom object. This reads a photo which is part of a list of
-        photos returned by the search method.""" 
-        attrs = xml.attributes.items() 
-        props = {}
-        for attr in attrs:
-            props[attr[0]] = attr[1]
-        coordinates = [float(props['longitude']), float(props['latitude'])]
-        props['img_url'] = "http://farm%s.static.flickr.com/%s/%s_%s_m.jpg" % (props['farm'], props['server'], props['id'], props['secret'])
-        del props['latitude']
-        del props['longitude']
-        return Feature(id=xml.getAttribute("id"), geometry={'type':"Point", 'coordinates':coordinates}, srs=self.srid_out, props=props)
-
-class API:
-    """Utility class for talking to Flickr API. This is a very lightweight
-       utility for this purpose, to save us from having to cart around another
-       library."""
-    def __init__(self, api_key = "065ba003282b719d84e2a322046c7364", secret = "a192dec51fb99499", url_base = "http://api.flickr.com/services/rest/"):
-        self.api_key = api_key
-        self.secret = secret
-        self.base = url_base
+            params = {'method' : 'flickr.photos.search','extras':'description,owner_name,geo,tags,license'}
+            
+            if action.bbox:
+                params['bbox'] = "%f,%f,%f,%f" % tuple(action.bbox)
+            
+            if hasattr(self, 'user_id'):
+                params['user_id'] = self.user_id
+                    
+            if hasattr(self, 'tags'):
+                params['tags'] = self.tags
+                if hasattr(self, 'tag_mode'):
+                    params['tag_mode'] = self.tag_mode
+                else:
+                    params['tag_mode'] = "any"
+            
+            data = self.api.request(params)
+            
+            doc = etree.parse(StringIO(data)).getroot()
+            photos = [ photo for photo in doc.xpath('/rsp/photos')[0] ]
     
-    def params_sig(self, params):
+            for photo in photos:
+                try:
+                    features.append(self.convert_photo(photo))
+                except Exception as e:
+                    continue
+        
+        return features
+
+    
+    def convert_photo (self, xml):
+        node_names = self.get_node_names(xml)
+        
+        props = {'img_url' : self.get_url(xml)}
+        
+        owners = xml.xpath('./owner')
+        if len(owners) > 0:
+            props['owner'] = owners[0].attrib['nsid']
+            props['username'] = owners[0].attrib['username']
+        for i in node_names:
+            if i == "tags":
+                tags = [ tag.text for tag in xml.xpath('./%s' % str(i))[0] ]
+                props[i] = ",".join(tags)
+                
+            else:
+                nodes = xml.xpath('./%s' % str(i))
+                if len(nodes) > 0:
+                    if len(list(nodes[0])) == 0:
+                        if nodes[0].text is None:
+                            props[i] = ""
+                        else:
+                            props[i] = nodes[0].text
+        try:
+            coordinates = self.get_coordinates(xml)
+        except:
+            raise
+    
+        return Feature(id=xml.attrib["id"], geometry={'type':"Point", 'coordinates':coordinates}, geometry_attr="geometry", srs=self.srid_out, props=props)
+
+    def get_node_names(self, xml):
+        if self.attributes == "*":
+            props = [ child.tag for child in xml ]
+        
+            props.remove("location")
+            props.remove("owner")
+        else:
+            props = self.attributes.split(',')
+        
+        return props
+
+    def get_coordinates(self, xml):
+        location = xml.xpath('./location')
+        if len(location) > 0:
+            loc = location[0]
+            return [float(loc.attrib['longitude']), float(loc.attrib['latitude'])]
+        
+        if "longitude" in xml.attrib and "latitude" in xml.attrib:
+            return [float(xml.attrib['longitude']), float(xml.attrib['latitude'])]
+
+
+        raise NoGeometryException("Twitter", self.name)
+
+    def get_url(self, xml):
+        return "http://farm%s.static.flickr.com/%s/%s_%s_b.jpg" % (xml.attrib['farm'], xml.attrib['server'], xml.attrib['id'], xml.attrib['secret'])
+
+
+
+
+class FlickrAPI:
+    
+    urls = {
+        'xml' : 'http://api.flickr.com/services/rest/'
+    }
+    
+    def __init__(self, api_key, api_secret):
+        self.api_key    = api_key
+        self.api_secret = api_secret
+
+    def request(self, params = {}, format = "rest"):
+        params['api_key'] = self.api_key
+        params['format'] = format
+        params['api_sig'] = self.signature(params)
+
+        return urllib.urlopen(self.urls["xml"], urllib.urlencode(params)).read()
+
+
+    def signature(self, params):
         items = []
         keys = params.keys()
         keys.sort()
-        for key in keys: 
+        for key in keys:
             items.append("%s%s" % (key,params[key]))
-        sign_string = "%s%s" % (self.secret, "".join(items))
+        sign_string = "%s%s" % (self.api_secret, "".join(items))
         return md5.md5(sign_string).hexdigest()
-    
-    def request(self, params):
-        params['api_key'] = self.api_key
-        sig = self.params_sig(params)
-        params['api_sig'] = sig
-        return urllib.urlopen(self.base, urllib.urlencode(params)).read()
-    
-    def fetch_frob(self):
-        data = self.request({'method':'flickr.auth.getFrob'})
-        doc = minidom.parseString(data)
-        self.frob = doc.getElementsByTagName("frob")[0].firstChild.nodeValue
-        return self.frob
-    
-    def get_link(self):
-        self.fetch_frob()
-        sig = self.params_sig({'api_key':self.api_key, 'perms':'read', 'frob': self.frob})
-        return "http://flickr.com/services/auth/?api_key=%s&perms=read&frob=%s&api_sig=%s" % ( self.api_key, self.frob, sig )
-    
-    def get_token(self):
-        params = {
-          'method':'flickr.auth.getToken',
-          'frob':self.frob
-        }
-        doc = minidom.parseString( self.request(params) )
-        return doc.getElementsByTagName("token")[0].firstChild.nodeValue
-        
-    
 
-if __name__ == "__main__":
-    a = API()
-    print "Open the following URL in a browser:" 
-    print a.get_link()
-    print "Press enter when complete."
-    raw_input()
-    token = a.get_token()
-    print "Your auth token is:\n\n    %s\n\nAdd the following line to a Flickr data source to complete configuration:\n\nauth_token=%s  " % (token,token)
+    
+    
