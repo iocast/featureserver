d8b51e3443a0536ffff596b466b8264bc359737c
iocast <iocast@me.com>
2012-11-16 11:16:50 +0100

    Fixed typo

--- FeatureServer/Exceptions/VersionNotSupportedException.py
+++ FeatureServer/Exceptions/VersionNotSupportedException.py
@@ -1,14 +1,14 @@
 '''
-    Created on October 15, 2012
+Created on November 16, 2012
     
-    @author: michel
-    '''
+@author: michel
+'''
 
 from FeatureServer.Exceptions.BaseException import BaseException
 
 class VersionNotSupportedException(BaseException):
     
-    message="Requested version '%s' is not supported: Use one of the following: %s"
+    message="Requested version '%s' is not supported. Use one of the following: %s"
     
     def __init__(self, locator, version, supported_versions, layer="", code="", message="", dump = ""):
         self.message = self.message % (version, ", ".join(supported_versions))
