2249c3579afc63419bcdea7c2f5225c0287913df
iocast <iocast@me.com>
2012-11-16 11:15:17 +0100

    Fixed typo

--- FeatureServer/Exceptions/LayerNotFoundException.py
+++ FeatureServer/Exceptions/LayerNotFoundException.py
@@ -10,8 +10,8 @@ class LayerNotFoundException(BaseException):
     
     message="Could not find the layer '%s': Check your config file for the missing layer. (Available layers are: %s)."
     
-    def __init__(self, locator, layer, layers, code="", message="", dump = ""):
-        self.message = self.message % (layer, ", ".join(layers))
+    def __init__(self, locator, layer, supported_layers, code="", message="", dump=""):
+        self.message = self.message % (layer, ", ".join(supported_layers))
         if len(message) > 0:
             self.message = message
         BaseException.__init__(self, self.message, code, locator, layer, dump)
