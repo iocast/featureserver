84c86b0a932e7ed13f4526bd476490be83038bc8
iocast <iocast@me.com>
2012-11-27 14:41:04 +0100

    rewrite to the new parser mechanism

--- FeatureServer/DataSource/PostGIS.py
+++ FeatureServer/DataSource/PostGIS.py
@@ -126,183 +126,99 @@ class PostGIS (DataSource):
         return self.table + "_" + self.fid_col + "_seq"
     
     def insert (self, action):
-        self.begin()
-        if action.feature != None:
-            feature = action.feature
-            columns = ", ".join(self.column_names(feature)+[self.getGeometry()])
-            values = ", ".join(self.value_formats(feature)+["SetSRID('%s'::geometry, %s) " % (WKT.to_wkt(feature.geometry), self.srid)])
-
-            sql = "INSERT INTO \"%s\" (%s) VALUES (%s)" % (self.table, columns, values)
-
-            cursor = self.db.cursor()
-            cursor.execute(str(sql), self.feature_values(feature))
-
-            cursor.execute("SELECT currval('%s');" % self.id_sequence())
-            action.id = cursor.fetchone()[0]
-            
-            return InsertResult(action.id, "")
+        sql = action.get_statement()
         
-        elif action.wfsrequest != None:
-            sql = action.wfsrequest.getStatement(self)
-            
-            cursor = self.db.cursor()
+        cursor = self.db.cursor()
+        try:
             cursor.execute(str(sql))
-            
-            cursor.execute("SELECT currval('%s');" % self.id_sequence())
-            action.id = cursor.fetchone()[0]
-            
-            return InsertResult(action.id, "")
-                        
-        return None
+        except Exception as e:
+            raise SyntaxException(locator = self.__class__.__name__, dump = str(e))
         
-
+        cursor.execute("SELECT currval('%s');" % self.id_sequence())
+        action.id =  cursor.fetchone()[0]
+        
+        return InsertResult(action.id, "")
+    
+    
     def update (self, action):
-        if action.feature != None:
-            feature = action.feature
-            predicates = ", ".join( self.feature_predicates(feature) )
-
-            sql = "UPDATE \"%s\" SET %s WHERE %s = %d" % ( self.table, predicates, self.fid_col, action.id )
-
-            cursor = self.db.cursor()
-            cursor.execute(str(sql), self.feature_values(feature))
-            
-            return UpdateResult(action.id, "")
+        sql = action.get_statement()
         
-        elif action.wfsrequest != None:
-            sql = action.wfsrequest.getStatement(self)
-            
-            cursor = self.db.cursor()
+        cursor = self.db.cursor()
+        
+        try:
             cursor.execute(str(sql))
+        except Exception as e:
+            raise SyntaxException(locator = self.__class__.__name__, dump = str(e))
+        
+        return UpdateResult(action.id, "")
 
-            return UpdateResult(action.id, "")
-            
-        return None
         
     def delete (self, action):
-        if action.feature != None:
-            sql = "DELETE FROM \"%s\" WHERE %s = %%(%s)d" % ( self.table, self.fid_col, self.fid_col )
-            cursor = self.db.cursor()
-            
-            try:
-                cursor.execute(str(sql) % {self.fid_col: action.id})
-            except:    
-                cursor.execute(str(sql), {self.fid_col: action.id})
-            
-            return DeleteResult(action.id, "")
+        sql = action.get_statement()
         
-        elif action.wfsrequest != None:
-            sql = action.wfsrequest.getStatement(self)
-            cursor = self.db.cursor()
-            try:
-                cursor.execute(str(sql) % {self.fid_col: action.id})
-            except:    
-                cursor.execute(str(sql), {self.fid_col: action.id})
-            
-            return DeleteResult(action.id, "")
-            
-        return None
+        cursor = self.db.cursor()
+        
+        try:
+            cursor.execute(str(sql))
+        except Exception as e:
+            raise SyntaxException(locator = self.__class__.__name__, dump = str(e))
 
-
+        return DeleteResult(action.id, "")
+    
+        
     def select (self, action):
         cursor = self.db.cursor()
+        
+        sql = "SELECT ST_AsText(ST_Transform(%s, %d)) as fs_text_geom, " % (self.getGeometry(), int(self.srid_out))
+        
+        # add attributes from config file
+        if hasattr(self, 'version'):
+            sql += "%s as version, " % self.version
+        if hasattr(self, 'ele'):
+            sql += "%s as ele, " % self.ele
 
-        if action.id is not None:
-            sql = "SELECT AsText(Transform(%s, %d)) as fs_text_geom, " % (self.getGeometry(), int(self.srid_out))
-            
-            if hasattr(self, 'version'):
-                sql += "%s as version, " % self.version
-            if hasattr(self, 'ele'):
-                sql += "%s as ele, " % self.ele
-            
-            sql += "\"%s\"" % self.fid_col
-            
-            if len(self.attribute_cols) > 0:
-                sql += ", %s" % self.attribute_cols
-            
-            if hasattr(self, "additional_cols"):
-                cols = self.additional_cols.split(';')
-                additional_col = ",".join(cols)
-                sql += ", %s" % additional_col
+        sql += "\"%s\"" % self.fid_col
 
-            sql += " FROM \"%s\" WHERE %s = %%(%s)s" % (self.table, self.fid_col, self.fid_col)
-            
-            #sql = "SELECT AsText(Transform(%s, %d)) as fs_text_geom, %s as ele, %s as version, \"%s, %s FROM \"%s\" WHERE %s = %%(%s)s" % (
-            #        self.getGeometry(), int(self.srid_out), self.ele, self.version, self.fid_col, self.attribute_cols, self.table, self.fid_col, self.fid_col )
+        if len(self.attribute_cols) > 0:
+            sql += ", %s" % self.attribute_cols
 
-            cursor.execute(str(sql), {self.fid_col: str(action.id)})
+        if hasattr(self, "additional_cols") and len(self.additional_cols) > 0:
+            cols = self.additional_cols.split(';')
+            additional_col = ",".join(cols)
+            sql += ", %s" % additional_col
 
-            result = [cursor.fetchone()]
-        else:
-            filters = []
-            attrs   = {}
-            if action.attributes:
-                match = Feature(props = action.attributes)
-                filters = self.feature_predicates(match)
-                for key, value in action.attributes.items():
-                    if isinstance(value, dict):
-                        attrs[key] = value['value']
-                    else:
-                        attrs[key] = value
-            if action.bbox:
-                filters.append( "%s && Transform(SetSRID('BOX3D(%f %f,%f %f)'::box3d, %s), %s) AND intersects(%s, Transform(SetSRID('BOX3D(%f %f,%f %f)'::box3d, %s), %s))" % (
-                                        (self.getGeometry(),) + tuple(action.bbox) + (self.srid_out,) + (self.srid,) + (self.getGeometry(),) + (tuple(action.bbox) + (self.srid_out,) + (self.srid,))))
-            sql = "SELECT AsText(Transform(%s, %d)) as fs_text_geom, " % (self.getGeometry(), int(self.srid_out))
-            if hasattr(self, 'ele'):
-                sql += "%s as ele, " % self.ele
-            if hasattr(self, 'version'):
-                sql += "%s as version, " % self.version
-            sql += "\"%s\"" % self.fid_col
-    
-            if len(self.attribute_cols) > 0:
-                sql += ", %s" % self.attribute_cols
-            
-            # check OGC FE attributes
-            if self.fe_attributes and action.wfsrequest:
-                fe_cols = action.wfsrequest.getAttributes()
+
+        # add attributes from parser
+        if self.fe_attributes:
+            if action.get_attributes() is not None and len(action.get_attributes()) > 0:
+                fe_cols = action.get_attributes()
                 ad_cols = self.getColumns()
-                
+                # removes attributes that already are defined in the configuration file
                 fe_cols = filter(lambda x: x not in ad_cols, fe_cols)
-                
+    
                 if len(fe_cols) > 0:
                     sql += ", %s" % ",".join(fe_cols)
-    
-            if hasattr(self, "additional_cols"):
-                cols = self.additional_cols.split(';')
-                additional_col = ",".join(cols)
-                sql += ", %s" % additional_col
 
-
-            sql += " FROM \"%s\"" % (self.table)
-            
-            #sql = "SELECT AsText(Transform(%s, %d)) as fs_text_geom, %s as ele, %s as version, \"%s\", %s FROM \"%s\"" % (self.getGeometry(), int(self.srid_out), self.ele, self.version, self.fid_col, self.attribute_cols, self.table)
-            if filters:
-                sql += " WHERE " + " AND ".join(filters)
-            if action.wfsrequest:
-                if filters:
-                    sql += " AND "
-                else:
-                    sql += " WHERE "
-                
-                sql += action.wfsrequest.render(self)
-                
-                
-            if self.order:
-                sql += " ORDER BY " + self.order
-            if action.maxfeatures:
-                sql += " LIMIT %d" % action.maxfeatures
-            #else:   
-            #    sql += " LIMIT 1000"
-            if action.startfeature:
-                sql += " OFFSET %d" % action.startfeature
-                        
-            try:
-                cursor.execute(str(sql), attrs)
-            except Exception, e:
-                if e.pgcode[:2] == errorcodes.CLASS_SYNTAX_ERROR_OR_ACCESS_RULE_VIOLATION:
-                    raise InvalidValueException(**{'dump':e.pgerror,'layer':self.name,'locator':'PostGIS'})
-                
-            result = cursor.fetchall() # should use fetchmany(action.maxfeatures)
+        sql += " FROM \"%s\"" % (self.table)
         
+        
+        if action.get_statement() is not None:
+            sql += " WHERE " + action.get_statement()
+                    
+        if self.order:
+            sql += " ORDER BY " + self.order
+            
+        print(str(sql))
+        
+        try:
+            cursor.execute(str(sql))
+        except Exception as e:
+            if e.pgcode[:2] == errorcodes.CLASS_SYNTAX_ERROR_OR_ACCESS_RULE_VIOLATION:
+                raise InvalidValueException(**{'dump':e.pgerror,'layer':self.name,'locator':'PostGIS'})
+            
+        result = cursor.fetchall()
+        
+
         columns = [desc[0] for desc in cursor.description]
         features = []
         for row in result:
@@ -316,19 +232,20 @@ class PostGIS (DataSource):
             del props['fs_text_geom']
             for key, value in props.items():
                 if isinstance(value, str): 
-                        props[key] = unicode(value, self.encoding)
+                    props[key] = unicode(value, self.encoding)
                 elif isinstance(value, datetime.datetime) or isinstance(value, datetime.date):
                     # stringify datetimes 
                     props[key] = str(value)
-                    
+
                 try:
                     if isinstance(value, decimal.Decimal):
-                            props[key] = unicode(str(value), self.encoding)
+                        props[key] = unicode(str(value), self.encoding)
                 except:
                     pass
-                    
+
             if (geom):
-                features.append( Feature( id, geom, self.getGeometry(), self.srid_out, props ) )
+                features.append( Feature( layer=action.layer, id=id, geometry=geom, geometry_attr=self.getGeometry(), srs=self.srid_out, props=props ) )
+
         return features
             
     def getColumns(self):
