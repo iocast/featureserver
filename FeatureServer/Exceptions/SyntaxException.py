e4a94c91bfea815e005b9764e00cbc9fc0d22c9d
iocast <iocast@me.com>
2012-11-27 14:39:10 +0100

    corrected string formatting

--- FeatureServer/Exceptions/SyntaxException.py
+++ FeatureServer/Exceptions/SyntaxException.py
@@ -5,7 +5,7 @@ class SyntaxException(BaseException):
     message="'%s' raised exception."
     
     def __init__(self, locator, layer="", code="", message="", dump = ""):
-        self.message = self.message % self.locator
+        self.message = self.message % locator
         if len(message) > 0:
             self.message = message
         BaseException.__init__(self, self.message, code, locator, layer, dump)
