50d35c1090ebb6efb6b122eb1b9f4d246c78194a
iocast <iocast@me.com>
2013-01-08 13:06:01 +0100

    predicate not found exception

--- featureserver/exceptions/datasource.py
+++ featureserver/exceptions/datasource.py
@@ -15,3 +15,15 @@ class ConnectionException(BaseException):
         if len(message) > 0:
             self.message = message
         BaseException.__init__(self, self.message, code, locator, layer, dump)
+
+class PredicateNotFoundException(BaseException):
+
+    message = "'%s' is not a valide predicate."
+
+    def __init__(self, locator, predicate, layer="", code="", message="", dump=""):
+        self.message = self.message % predicate
+        if len(message) > 0:
+            self.message = message
+        BaseException.__init__(self, self.message, code, locator, layer, dump)
+
+
