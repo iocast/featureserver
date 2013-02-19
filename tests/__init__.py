687dfcfc93a30dc1af27c5338fd22380c3008882
icoast <iocast@me.com>
2013-02-19 09:12:14 +0100

    full test

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,15 +3,18 @@ from unittest import TestSuite
 import wfs_base
 
 import ds_spatialite, ds_postgis, ds_postgishstore
+import ds_geoalchemy
 
 def test_suite():
     suite = TestSuite()
     
-    #suite.addTest(wfs_base.test_suite())
+    suite.addTest(wfs_base.test_suite())
     
-    #suite.addTests(ds_spatialite.test_suites())
-    #suite.addTests(ds_postgis.test_suites())
+    suite.addTests(ds_spatialite.test_suites())
+    suite.addTests(ds_postgis.test_suites())
     suite.addTests(ds_postgishstore.test_suites())
+    
+    suite.addTest(ds_geoalchemy.test_suite())
 
 
     return suite
